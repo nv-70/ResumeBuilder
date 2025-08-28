1d601b3e129915f20f58224a43a1fd8d574e3cd6
Nitin Verma <nv571020@gmail.com>
2025-08-28 14:28:08 +0530

    rebuild frontend due to some issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const App = () => {
+function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
-  );
+  )
 }
 
 export default App
