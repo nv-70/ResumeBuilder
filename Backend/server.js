5e07bca6742444e529f17c592e64342551fe01ba
Nitin Verma <nv571020@gmail.com>
2025-08-28 14:23:04 +0530

    feat(server): serve uploaded images statically with CORS headers.

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,18 @@
-import express from "express"
-import cors from "cors"
-import 'dotenv/config'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
 import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+import resumeRoutes from "./routes/resumeRoutes.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
-const PORT =  4000;
+const PORT = 4000;
 app.use(cors());
 
 //CONNECT DB
@@ -15,11 +22,22 @@ connectDB();
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
+app.use("/api/resume", resumeRoutes);
 
-app.get('/',(req,res)=>{
-    res.send("API WORKING")
-})
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "uploads"), {
+    setHeaders: (res, _path) => {
+      res.set("Access-Control-Allow-Origin", "http://localhost:5173");
+    },
+  })
+);
 
-app.listen(PORT,()=>{
-    console.log(`Server started on http://localhost:${PORT}`)
-})
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
