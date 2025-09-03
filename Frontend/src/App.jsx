82a0102597d1cc3682ec79a243674bef5980d78b
Nitin Verma <nv571020@gmail.com>
2025-09-03 18:36:29 +0530

    chore: wrap app with UserProvider for global user state

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import LandingPage from './pages/landingPage'
+import UserProvider from './context/userContext'
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-    </Routes>
+    <UserProvider>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </UserProvider>
   )
 }
 
