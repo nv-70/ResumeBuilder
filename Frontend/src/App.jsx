3a058d7df3ca1c067b2f2bf45067c718a9d5d948
Nitin Verma <nv571020@gmail.com>
2025-09-17 13:45:06 +0530

    correct  error

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/landingPage";
 import UserProvider from "./context/userContext";
-import Dashboard from "./pages/dashboard";
+import Dashboard from "./pages/Dashboard";
 import EditResume from "./Components/EditResume";
 import { Toaster } from "react-hot-toast";
 function App() {
@@ -11,17 +11,17 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path= '/resume/:resumeId' element={<EditResume/>} />
+        <Route path="/resume/:resumeId" element={<EditResume />} />
       </Routes>
 
-      <Toaster toastOptions={{
-        className: "",
-        style:{
-          fontSize: "13px"
-        }
-      }}>
-
-      </Toaster>
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      ></Toaster>
     </UserProvider>
   );
 }
